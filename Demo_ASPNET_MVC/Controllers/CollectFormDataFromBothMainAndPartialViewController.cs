4c89e83506ee7bc35e0abf7da8f16a25034797c1
Vimal Gohil <gohil.vml2008@gmail.com>
2022-10-08 13:27:59 +0530

    Demo of collect form data from both Main and Partial View in Asp.Net MVC

---
+++ Demo_ASPNET_MVC/Controllers/CollectFormDataFromBothMainAndPartialViewController.cs
@@ -0,0 +1,20 @@
+﻿using Demo_ASPNET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace Demo_ASPNET_MVC.Controllers
+{
+    public class CollectFormDataFromBothMainAndPartialViewController : Controller
+    {
+        public IActionResult CollectFormDataFromBothMainAndPartialView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void GetFormDataFromMainAndPartialView(Employee empFormData)
+        {
+
+        }
+    }
+}
---
+++ Demo_ASPNET_MVC/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo_ASPNET_MVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeCode { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public bool IsActive { get; set; }
+    }
+}
